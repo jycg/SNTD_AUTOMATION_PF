edef817f48d810079287aa6c742a4cd53febfede
jgarcia <julio_salchi@live.com.mx>
2024-01-16 17:31:34 -0600

    Nuevo archivo para generar los pasos de la solicitud de crédito.

--- AUTO/Test/SOL_DATOS_PERSONALES.py
+++ AUTO/Test/SOL_DATOS_PERSONALES.py
@@ -1,24 +1,4 @@
 import unittest
-import re
-import time
-import sys
-import os
-import xlrd
-import logging
-import traceback
-from datetime import datetime
-from selenium import webdriver
-from selenium.common import TimeoutException, NoSuchElementException
-from selenium.webdriver.common.by import By
-from AUTO.PageApp.loginPage import LoginPage
-from AUTO.PageApp.searchPage import SearchPage
-from AUTO.PageApp.quotePage import QuotePage
-from selenium.webdriver.edge.service import Service as EdgeService
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from faker import Faker
-from AUTO.Locator.locators import Locators
-from selenium.webdriver.edge.options import Options
 
 
 class MyTestCaseSolicitud(unittest.TestCase):
@@ -26,7 +6,7 @@ class MyTestCaseSolicitud(unittest.TestCase):
 
     @classmethod
     def datos_personales(cls):
-        print("Ejemplo de una función diferente en diferente archivo")
+        print("Ejemplo")
 
 
 if __name__ == '__main__':
